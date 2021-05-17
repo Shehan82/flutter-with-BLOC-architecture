cfe8100af164126740899a2275a4fd799586d48a
shehan82 <shehansandeepa82@gmail.com>
2021-05-17 18:04:59 +0530

    create third Screen

---
+++ lib/presentation/screens/thirdScreen.dart
@@ -0,0 +1,83 @@
+import 'package:bloc_test/logic/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ThirdScreen extends StatefulWidget {
+  ThirdScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ThirdScreenState createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                print("helloo i am block listner");
+
+                if (state.isIncrement == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('incremented!'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('decremented!'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                print("helloo i am block bulider");
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Container(
+              width: 320,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increament();
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).decreament();
+                      },
+                      tooltip: 'Decrement',
+                      child: Icon(Icons.remove)),
+                ],
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.amber,
+              child: Text("Go to second screen"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
