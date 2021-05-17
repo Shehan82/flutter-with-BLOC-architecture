a9db47f8bccce38933eb97a132d562f79e655ec5
shehan82 <shehansandeepa82@gmail.com>
2021-05-17 14:22:32 +0530

    added second screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:bloc_test/cubit/counter_cubit.dart';
+import 'package:bloc_test/presentation/screens/homeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'logic/cubit/counter_cubit.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,80 +19,6 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
         ),
         home: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.isIncrement == true) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('incremented!'),
-              duration: Duration(milliseconds: 300),
-            ));
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text('decremented!'),
-              duration: Duration(milliseconds: 300),
-            ));
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                },
-              ),
-              Container(
-                width: 320,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).increament();
-                      },
-                      tooltip: 'Increment',
-                      child: Icon(Icons.add),
-                    ),
-                    FloatingActionButton(
-                        onPressed: () {
-                          BlocProvider.of<CounterCubit>(context).decreament();
-                        },
-                        tooltip: 'Decrement',
-                        child: Icon(Icons.remove)),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
       ),
     );
   }
---
+++ lib/presentation/screens/homeScreen.dart
@@ -0,0 +1,78 @@
+import 'package:bloc_test/logic/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                print("helloo i am block listner");
+
+                if (state.isIncrement == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('incremented!'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('decremented!'),
+                    duration: Duration(milliseconds: 300),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                print("helloo i am block bulider");
+                return Text(
+                  state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Container(
+              width: 320,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increament();
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  ),
+                  FloatingActionButton(
+                      onPressed: () {
+                        BlocProvider.of<CounterCubit>(context).decreament();
+                      },
+                      tooltip: 'Decrement',
+                      child: Icon(Icons.remove)),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
